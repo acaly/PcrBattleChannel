17747a83a768468a86c7e309e7e4df512abc4419
Zhenwei Wu <wuzhenwei35@gmail.com>
2021-01-25 10:13:35 -0500

    Allow admin to login as other users.

---
+++ Pages/Admin/SuLogin.cshtml.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using PcrBattleChannel.Data;
+using PcrBattleChannel.Models;
+
+namespace PcrBattleChannel.Pages.Admin
+{
+    [Authorize(Roles = "Admin")]
+    public class SuLoginModel : PageModel
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly SignInManager<PcrIdentityUser> _signInManager;
+
+        public SuLoginModel(ApplicationDbContext context, SignInManager<PcrIdentityUser> signInManager)
+        {
+            _context = context;
+            _signInManager = signInManager;
+        }
+
+        [BindProperty]
+        public string Email { get; set; }
+
+        public void OnGet()
+        {
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            var u = await _context.Users.FirstOrDefaultAsync(u => u.Email == Email);
+            await _signInManager.SignInAsync(u, false);
+            return RedirectToPage("/Home/Index");
+        }
+    }
+}
